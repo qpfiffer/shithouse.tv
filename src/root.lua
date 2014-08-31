7a1a664c82d91e7548239d9807203064dbcbe2e2
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-08-31 23:33:37 +0800

    Hey look, branching

--- src/root.lua
+++ src/root.lua
@@ -1,6 +1,25 @@
 -- Well, this is pretty neat.
+local HOST = "shitless.com"
+function bump(hostname)
+    local subdomain = string.match(hostname, "[a-zA-Z]*")
+
+    if subdomain == string.match(HOST, "[a-zA-Z]*") then
+        local f = io.open("templates/index.html", "r")
+        print(f:read("*all"))
+        f:close()
+        return
+    end
+
+    local f = io.open("templates/bump.html", "r")
+    print(f:read("*all"))
+    f:close()
+
+end
+
 function root()
-    if arg[2] ~= "shithouse.tv" then
+    if arg[2] ~= HOST then
+        bump(arg[2])
+    else
         local f = io.open("templates/index.html", "r")
         print(f:read("*all"))
         f:close()
