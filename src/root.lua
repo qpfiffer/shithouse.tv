0a9665a7217f5ef4f9bd085fa264b77c1d8bb93c
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-08-31 22:54:01 +0800

    Yeah sure whatever

--- src/root.lua
+++ src/root.lua
@@ -1,25 +1,10 @@
 -- Well, this is pretty neat.
-function main_page(func)
-    print([[
-<!DOCTYPE html>
-<html>
-    <body>]])
-    func()
-    print([[
-    </body>
-</html>]])
-end
-
 function root()
     if arg[2] ~= "shithouse.tv" then
-        print([[
-        <form method="POST">
-            <label for="subdomain">Subdomain:</label>
-            <input id="subdomain" />
-            <input type="submit" />
-        </form>
-]])
+        local f = io.open("templates/index.html", "r")
+        print(f:read("*all"))
+        f:close()
     end
 end
 
-main_page(root)
+root()
