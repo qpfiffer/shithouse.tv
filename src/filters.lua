eba5cd546fa3c5638857c9be4235909aed9fc00c
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-09-01 23:48:19 +0800

    huehuehueuhe

--- src/filters.lua
+++ src/filters.lua
@@ -1,9 +1,23 @@
 local filters_module = {}
+local normal = [[0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~]]
+local wide = [[０１２３４５６７８９ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ！゛＃＄％＆（）＊＋、ー。／：；〈＝〉？＠［\\］＾＿‘｛｜｝"]]
 
-filters_module.filter_pattern = "yYy ([a-zA-Z]*) (.*) yYy"
+filters_module.filter_pattern = "yYy ([a-zA-Z]*) ([a-zA-Z :']*) yYy"
 
 function filters_module.fullwidth(text)
-    return string.upper(text)
+    local new_str = ""
+
+    for c in text:gmatch(".") do
+        local idx = string.find(normal, c)
+        if idx ~= nil then
+            local wide_idx = (idx * 3) - 3
+            new_str = new_str .. wide:sub(wide_idx + 1, wide_idx + 3)
+        else
+            new_str = new_str .. c
+        end
+    end
+
+    return new_str
 end
 
 return filters_module
