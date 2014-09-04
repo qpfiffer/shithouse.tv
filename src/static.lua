2882ef563e51f07407e1b0d31083d3a643e048d5
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-09-04 21:28:56 +0800

    ahahaha, it works

--- src/static.lua
+++ src/static.lua
@@ -1,7 +1,9 @@
 local config = require "src/config"
 local utils = require "src/utils"
+local fuck_json = require "src/JSON"
 
 function static(uri)
+    local filename = utils.get_file_name_from_path(uri)
     local subdomain_arg = string.match(arg[2], "[a-zA-Z]*")
     local meta_data = utils.check_for_bump(subdomain_arg)
 
@@ -11,7 +13,15 @@ function static(uri)
 
     -- TODO: Search through metadata for assoc. files and return the one
     -- requested, otherwise 404.
-    io.close(meta_data)
+    local decoded = fuck_json:decode(meta_data:read("*all"))
+    meta_data:close()
+    if filename == decoded["image"] then
+        local path = utils.build_bump_path(subdomain_arg)
+        local f = assert(io.open(path .. "/" .. filename))
+        print(f:read("*all"))
+        f:close()
+        return
+    end
     return utils.error404()
 end
 
--- src/utils.lua
+++ src/utils.lua
@@ -1,12 +1,16 @@
 local utils_module = {}
 local config = require "src/config"
 
+function utils_module.build_bump_path(name)
+    return config.BUMPS .. "/" .. name
+end
+
 function utils_module.check_for_bump(name)
     if not name then
         return nil
     end
 
-    local meta_data = io.open(config.BUMPS .. "/" .. name .. "/" .. config.MD_NAME)
+    local meta_data = io.open(utils_module.build_bump_path(name) .. "/" .. config.MD_NAME)
     return meta_data
 end
 
